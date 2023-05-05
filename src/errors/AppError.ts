ea8e14c0d40e3901a4817231ae643e2bc1fd831b
Ignacio Codina <ext_iandcodinap@falabella.cl>
2023-05-04 22:26:12 -0400

    errors

---
+++ src/errors/AppError.ts
@@ -0,0 +1,14 @@
+class AppError extends Error {
+    public readonly statusCode: number;
+    public readonly message: string;
+    public readonly metadata: unknown;
+
+    constructor(statusCode: number, message: string, metadata?: unknown) {
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.metadata = metadata;
+    }
+}
+
+export default AppError;
---
+++ src/errors/ErrorMiddleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+import AppError from './AppError';
+
+export class ErrorMiddleware {
+    public static handle(error: AppError, req: Request, res: Response, next: NextFunction) {
+        if (error instanceof AppError) {
+            res.status(error.statusCode).json({
+                error: error.message,
+                status: error.statusCode,
+            });
+        } else {
+            res.status(500).json({
+                error: 'Internal server error',
+            });
+        }
+        next();
+    }
+}
