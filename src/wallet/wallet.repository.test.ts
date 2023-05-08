83e7a20db0e9709d07e64f0f74c29d22610a67d3
Ignacio Codina <ext_iandcodinap@falabella.cl>
2023-05-08 08:57:13 -0400

    jest and createWallet mock

---
+++ src/wallet/wallet.repository.test.ts
@@ -0,0 +1,65 @@
+import { WalletRepositoryDB } from './wallet.repository';
+import { pool } from '../config/database';
+import { NewWallet } from './wallet.model';
+import AppError from '../errors/AppError';
+
+jest.mock('../config/database');
+
+describe('WalletRepositoryDB', () => {
+    let walletRepository: WalletRepositoryDB;
+
+    beforeEach(() => {
+        walletRepository = new WalletRepositoryDB();
+        (pool.execute as jest.Mock).mockClear();
+    });
+
+    describe('createWallet', () => {
+        it('should create a new wallet and return it', async () => {
+            const newWallet: NewWallet = {
+                balance: 100,
+                currency: 'CLP',
+                userId: 1,
+            };
+
+            (pool.execute as jest.Mock).mockResolvedValue([
+                {
+                    affectedRows: 1,
+                    insertId: '1',
+                },
+            ]);
+
+            const wallet = await walletRepository.createWallet(newWallet);
+
+            expect(wallet).toEqual({
+                id: '1',
+                ...newWallet,
+            });
+
+            expect(pool.execute).toHaveBeenCalledWith(
+                'INSERT INTO wallet (balance, currency, user_id) VALUES (?, ?, ?)',
+                [newWallet.balance, newWallet.currency, newWallet.userId]
+            );
+        });
+
+        it('should throw an AppError if wallet creation fails', async () => {
+            const newWallet: NewWallet = {
+                balance: 100,
+                currency: 'CLP',
+                userId: 1,
+            };
+
+            (pool.execute as jest.Mock).mockResolvedValue([
+                {
+                    affectedRows: 0,
+                },
+            ]);
+
+            await expect(walletRepository.createWallet(newWallet)).rejects.toThrow(AppError);
+
+            expect(pool.execute).toHaveBeenCalledWith(
+                'INSERT INTO wallet (balance, currency, user_id) VALUES (?, ?, ?)',
+                [newWallet.balance, newWallet.currency, newWallet.userId]
+            );
+        });
+    });
+});
