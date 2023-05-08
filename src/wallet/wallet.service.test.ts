49b0fb8229d8a7e90eace342952299f1075c03af
Ignacio Codina <ext_iandcodinap@falabella.cl>
2023-05-08 10:59:37 -0400

    added wallet service tests

--- src/wallet/wallet.service.test.ts
+++ src/wallet/wallet.service.test.ts
@@ -1,5 +1,6 @@
 import { IWalletRepository } from './wallet.repository';
 import { WalletService } from './wallet.service';
+import { NewWallet, Wallet } from './wallet.model';
 
 const mockWalletRepository: jest.Mocked<IWalletRepository> = {
   createWallet: jest.fn(),
@@ -12,5 +13,47 @@ describe('WalletService', () => {
   beforeEach(() => {
     jest.clearAllMocks();
     walletService = new WalletService(mockWalletRepository);
+  });
+
+    describe('createWallet', () => {
+    it('should call the wallet repository and return a new wallet', async () => {
+      const newWallet: NewWallet = {
+        balance: 100,
+        currency: 'CLP',
+        userId: 1,
+      };
+
+      const createdWallet: Wallet = {
+        id: 1,
+        ...newWallet,
+      };
+
+      mockWalletRepository.createWallet.mockResolvedValue(createdWallet);
+
+      const result = await walletService.createWallet(newWallet);
+
+      expect(result).toEqual(createdWallet);
+      expect(mockWalletRepository.createWallet).toHaveBeenCalledWith(newWallet);
+    });
+  });
+
+  describe('getWalletByUserId', () => {
+    it('should call the wallet repository and return a wallet for the given user ID', async () => {
+      const userId = 1;
+      const wallet: Wallet = {
+        id: 1,
+        balance: 100,
+        currency: 'CLP',
+        userId,
+      };
+      const userIdStr = userId.toString();
+
+      mockWalletRepository.getWalletByUserId.mockResolvedValue(wallet);
+
+      const result = await walletService.getWalletByUserId(userIdStr);
+
+      expect(result).toEqual(wallet);
+      expect(mockWalletRepository.getWalletByUserId).toHaveBeenCalledWith(userIdStr);
+    });
   });
 });
